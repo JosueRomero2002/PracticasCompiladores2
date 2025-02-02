f5230ace829b73709bb1520d72e065325f60de31
josueromero2002 <josuepinro21@unitec.edu>
2025-02-02 16:07:18 -0600

    first commit

---
+++ build/CMakeFiles/MiniJavaLexer.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiniJavaLexer.dir/MiniJavaLexer.cpp.o"
+  "CMakeFiles/MiniJavaLexer.dir/MiniJavaLexer.cpp.o.d"
+  "CMakeFiles/MiniJavaLexer.dir/MiniJavaParser.cpp.o"
+  "CMakeFiles/MiniJavaLexer.dir/MiniJavaParser.cpp.o.d"
+  "CMakeFiles/MiniJavaLexer.dir/main.cpp.o"
+  "CMakeFiles/MiniJavaLexer.dir/main.cpp.o.d"
+  "MiniJavaLexer"
+  "MiniJavaLexer.cpp"
+  "MiniJavaLexer.hpp"
+  "MiniJavaLexer.pdb"
+  "MiniJavaParser.cpp"
+  "MiniJavaParser.hpp"
+  "tokens.hpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiniJavaLexer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
